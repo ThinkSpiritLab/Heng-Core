0854e93c971011d4ef526ce692508112e01b332f
HuGuang@TestServer <1395943920@qq.com>
2021-01-25 20:52:36 +0800

    chain stytle in addArg

--- src/main.cpp
+++ src/main.cpp
@@ -10,31 +10,30 @@ using Cfg       = HengCore::Config::Config;
 using Excutable = HengCore::Excutable;
 int main(int argc, char *argv[])
 {
-    auto praser = ArgvPraser::OptionPraser<Cfg>();
-    praser.add("tl", &Cfg::timeLimit);
-    praser.add("t", &Cfg::timeLimit);
-    praser.add("ml", &Cfg::memLimit);
-    praser.add("m", &Cfg::memLimit);
-    // praser.add("ol", &Cfg::outPutLimit);
-    praser.add("uid", &Cfg::uid);
-    praser.add("u", &Cfg::uid);
-    praser.add("gid", &Cfg::gid);
-    praser.add("g", &Cfg::gid);
-    praser.add("pidl", &Cfg::maxPid);
-    praser.add("p", &Cfg::maxPid);
-    praser.add("stdin", &Cfg::stdin);
-    praser.add("i", &Cfg::stdin);
-    praser.add("stdout", &Cfg::stdout);
-    praser.add("o", &Cfg::stdout);
-    praser.add("stderr", &Cfg::stderr);
-    praser.add("e", &Cfg::stderr);
-    praser.add("f", &Cfg::outFd);
-    praser.add("cwd", &Cfg::cwd);
-    praser.add("c", &Cfg::cwd);
-    praser.add("args", &Cfg::args);
-    praser.add("a", &Cfg::args);
-    praser.add("bin", &Cfg::bin, true);
-    auto      arg = praser.prase(argc, argv);
+    auto arg = ArgvPraser::OptionPraser<Cfg>()
+                 .add("tl", &Cfg::timeLimit)
+                 .add("t", &Cfg::timeLimit)
+                 .add("ml", &Cfg::memLimit)
+                 .add("m", &Cfg::memLimit)
+                 .add("uid", &Cfg::uid)
+                 .add("u", &Cfg::uid)
+                 .add("gid", &Cfg::gid)
+                 .add("g", &Cfg::gid)
+                 .add("pidl", &Cfg::maxPid)
+                 .add("p", &Cfg::maxPid)
+                 .add("stdin", &Cfg::stdin)
+                 .add("i", &Cfg::stdin)
+                 .add("stdout", &Cfg::stdout)
+                 .add("o", &Cfg::stdout)
+                 .add("stderr", &Cfg::stderr)
+                 .add("e", &Cfg::stderr)
+                 .add("f", &Cfg::outFd)
+                 .add("cwd", &Cfg::cwd)
+                 .add("c", &Cfg::cwd)
+                 .add("args", &Cfg::args)
+                 .add("a", &Cfg::args)
+                 .add("bin", &Cfg::bin, true)
+                 .prase(argc, argv);
     Excutable excutable(arg);
 #ifdef DEBUG
     std::cout << nlohmann::json(arg).dump(4) << std::endl;
